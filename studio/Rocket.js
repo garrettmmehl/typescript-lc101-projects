de212f8de1c25bd76bf4d288eb9de5422cc9ad2e
garrettmmehl <garrettmmehl@gmail.com>
2019-12-09 20:05:13 -0600

    worked through studio exercises

---
+++ studio/Rocket.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+        this.cargoItems = [];
+        this.astronauts = [];
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var combinedMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            combinedMass += items[i].massKg;
+        }
+        return combinedMass;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if ((this.currentMassKg() + item.massKg) <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
